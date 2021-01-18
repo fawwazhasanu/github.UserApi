1f66fb80c8a4758bc5a0ef46856697ad29b8dc13
Fawwaz Hasanu <aan.canditegal@gmail.com>
2021-01-18 20:04:41 +0700

    Fawwaz

---
+++ app/src/main/java/com/github/api/data/UserList.kt
@@ -0,0 +1,12 @@
+package com.github.api.data
+
+import com.google.gson.annotations.SerializedName
+
+class UserList (
+        @SerializedName("incomplete_results")
+        val incompleteResults: Boolean,
+        @SerializedName("items")
+        val items: List<User>,
+        @SerializedName("total_count")
+        val totalCount: Int
+)
